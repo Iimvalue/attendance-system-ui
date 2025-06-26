fff760fd5b5939a10598c5e5d0327e20e2050d1d
Abdulrahman <1xvlux1@gmail.com>
2025-06-26 10:27:11 +0300

    Update

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
       { path: "principle", element: <PrincipleDashboard /> },
       { path: "teacher", element: <TeacherDashboard /> },
       { path: "student", element: <StudentDashboard /> },
-      { path: "login", element: <LoginPage /> },
+      // { path: "login", element: <LoginPage /> },
     ],
   },
 ]);
